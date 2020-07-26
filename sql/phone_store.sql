9cfad4d8a691cd7dc2646004df2230076534d471
que2017 <1563645183@qq.com>
2020-07-26 20:24:55 +0800

    完成手机规格数据表及单元测试

--- sql/phone_store.sql
+++ sql/phone_store.sql
@@ -41,3 +41,20 @@ CREATE TABLE `phone_info` (
     PRIMARY KEY (`phone_id`)
 ) COMMENT='商品表';
 
+# 创建表: 手机规格
+DROP TABLE IF EXISTS `phone_specs`;
+# /*!40101 SET @saved_cs_client     = @@character_set_client */;
+# SET character_set_client = utf8mb4 ;
+CREATE TABLE `phone_specs` (
+    `specs_id` int(11) NOT NULL AUTO_INCREMENT,
+    `phone_id` varchar(32) COLLATE utf8mb4_general_ci NOT NULL,
+    `specs_name` varchar(64) COLLATE utf8mb4_general_ci NOT NULL COMMENT '规格名称',
+    `specs_stock` int(11) NOT NULL COMMENT '库存',
+    `specs_price` decimal(8,2) NOT NULL COMMENT '单价',
+    `specs_icon` varchar(512) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '小图',
+    `specs_preview` varchar(512) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '预览图',
+    `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+    `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    PRIMARY KEY (`specs_id`)
+) COMMENT='商品规格表';
+#) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='商品规格表';
